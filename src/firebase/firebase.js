737b76e862111f6de08b1a58c6603c114e03eb54
kaydo1506 <okedoifeoma@yahoo.com>
2022-01-26 14:18:31 +0100

    removed unused vars from firebase file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getDatabase, onValue, ref } from 'firebase/database';
-import { GoogleAuthProvider, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: 'AIzaSyDGQvukIG0HpjYNMNsIeEc8xOIaUV7c87w',
