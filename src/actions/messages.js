358279e536aae2517320e208769d5fd992a014db
kaydo1506 <okedoifeoma@yahoo.com>
2022-02-09 19:13:55 +0100

    removed unnecessary comment

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -1,7 +1,5 @@
 import { db } from '../firebase/firebase';
 import { ref, push, onValue } from 'firebase/database';
-// import { onAuthStateChanged } from 'firebase/auth';
-// import { auth } from '../firebase/firebase';
 
 export const addMessage = (chatMessage) => ({
     type: 'ADD_MESSAGE',
